f37dc5586c0ffb4386b54214f07392c72976512f
Redcroow <kilian.mvrl@gmail.com>
2024-04-09 11:35:37 +0200

    feat(dev): create component for homepage admini card

--- src/pages/HomepageAdmin/HomepageAdmin.tsx
+++ src/pages/HomepageAdmin/HomepageAdmin.tsx
@@ -1,56 +1,57 @@
 import React from 'react';
-import { IonPage, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonGrid, IonRow, IonCol, IonImg } from '@ionic/react';
+import { IonPage, IonContent, IonGrid, IonRow, IonCol } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
 import './HomepageAdmin.scss';
 import HeaderAdmin from '../../components/Header/Admin/HeaderAdmin';
+import HomepageCard from '../../components/HomepageCard/HomepageCard';
 
 const HomepageAdmin: React.FC = () => {
-    const history = useHistory();
+  const history = useHistory();
 
-    const navigateToPage = (page: string) => {
-        history.push(`/${page}`);
-    };
+  const navigateToPage = (page: string) => {
+    history.push(`/${page}`);
+  };
 
-    return (
-        <IonPage>
-            <HeaderAdmin />
-            <IonContent>
-                <div className="homepage-container">
-                    <IonGrid>
-                        <IonRow>
-                            <IonCol size="12" size-md="4">
-                                <IonCard className="homepage-card" onClick={() => navigateToPage('flotte')}>
-                                    <IonImg src="src/assets/images/ma-flotte.png" alt="Ma flotte" />
-                                    <IonCardHeader>
-                                        <IonCardSubtitle>Explorez</IonCardSubtitle>
-                                        <IonCardTitle>Ma flotte</IonCardTitle>
-                                    </IonCardHeader>
-                                </IonCard>
-                            </IonCol>
-                            <IonCol size="12" size-md="4">
-                                <IonCard className="homepage-card" onClick={() => navigateToPage('employers')}>
-                                    <IonImg src="src/assets/images/mes-employers.png" alt="Les employers" />
-                                    <IonCardHeader>
-                                        <IonCardSubtitle>Gérez</IonCardSubtitle>
-                                        <IonCardTitle>Les employers</IonCardTitle>
-                                    </IonCardHeader>
-                                </IonCard>
-                            </IonCol>
-                            <IonCol size="12" size-md="4">
-                                <IonCard className="homepage-card" onClick={() => navigateToPage('factures')}>
-                                    <IonImg src="src/assets/images/mes-factures.png" alt="Les factures" />
-                                    <IonCardHeader>
-                                        <IonCardSubtitle>Gérez</IonCardSubtitle>
-                                        <IonCardTitle>Les factures</IonCardTitle>
-                                    </IonCardHeader>
-                                </IonCard>
-                            </IonCol>
-                        </IonRow>
-                    </IonGrid>
-                </div>
-            </IonContent>
-        </IonPage>
-    );
+  return (
+    <IonPage>
+      <HeaderAdmin />
+      <IonContent>
+        <div className="homepage-container">
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12" size-md="4">
+                <HomepageCard
+                  imageSrc="src/assets/images/ma-flotte.png"
+                  alt="Ma flotte"
+                  subtitle="Explorez"
+                  title="Ma flotte"
+                  onClick={() => navigateToPage('flotte')}
+                />
+              </IonCol>
+              <IonCol size="12" size-md="4">
+                <HomepageCard
+                  imageSrc="src/assets/images/mes-employers.png"
+                  alt="Les employers"
+                  subtitle="Gérez"
+                  title="Les employers"
+                  onClick={() => navigateToPage('employers')}
+                />
+              </IonCol>
+              <IonCol size="12" size-md="4">
+                <HomepageCard
+                  imageSrc="src/assets/images/mes-factures.png"
+                  alt="Les factures"
+                  subtitle="Gérez"
+                  title="Les factures"
+                  onClick={() => navigateToPage('factures')}
+                />
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default HomepageAdmin;
