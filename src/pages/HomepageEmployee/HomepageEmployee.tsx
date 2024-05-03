62f806211db0c8d9a335bc205e2de60d06c05b37
Redcroow <kilian.mvrl@gmail.com>
2024-05-03 11:44:00 +0200

    feat(dev): rename some file + add scss + finish animation

--- src/pages/HomepageEmployee/HomepageEmployee.tsx
+++ src/pages/HomepageEmployee/HomepageEmployee.tsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { IonContent, IonPage } from '@ionic/react';
 import HeaderEmployee from '../../components/Header/Employee/HeaderEmployee';
+import './HomepageEmployee.scss';
 
 const HomepageEmployee: React.FC = () => {
   const [isVideoPlayed, setIsVideoPlayed] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
 
   useEffect(() => {
     const isVideoPlayedInSession = sessionStorage.getItem('isVideoPlayed');
+
     if (isVideoPlayedInSession === 'true') {
       setIsVideoPlayed(true);
+
     } else {
       const videoElement = document.getElementById('presentation-video') as HTMLVideoElement;
 
       const handleVideoEnd = () => {
-        setIsVideoPlayed(true);
-        sessionStorage.setItem('isVideoPlayed', 'true');
+        setFadeOut(true);
+        setTimeout(() => {
+          setIsVideoPlayed(true);
+          sessionStorage.setItem('isVideoPlayed', 'true');
+        }, 1500);
       };
+      
 
       if (videoElement) {
         videoElement.play().catch(error => console.error('Error playing video:', error));
@@ -31,33 +39,30 @@ const HomepageEmployee: React.FC = () => {
   }, []);
 
   return (
-    <IonPage>
-      <div style={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}>
-        {!isVideoPlayed && (
-          <video
-            id="presentation-video"
-            style={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              minWidth: '100%',
-              minHeight: '100%',
-              width: 'auto',
-              height: 'auto',
-            }}
-            autoPlay
-            muted
-          >
-            <source src="src/assets/video/homepage-pres.mp4" type="video/mp4" />
-          </video>
-        )}
-        {isVideoPlayed && (
-          <IonContent>
-            <HeaderEmployee />
-          </IonContent>
-        )}
-      </div>
+    <IonPage className='employee-page'>
+      <video
+        id="presentation-video"
+        className={fadeOut ? 'fade-out-employee' : ''}
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          minWidth: '100%',
+          minHeight: '100%',
+          width: 'auto',
+          height: 'auto',
+        }}
+        autoPlay
+        muted
+      >
+        <source src="src/assets/video/homepage-employee-pres.mp4" type="video/mp4" />
+      </video>
+      {isVideoPlayed && (
+        <IonContent>
+          <HeaderEmployee />
+        </IonContent>
+      )}
     </IonPage>
   );
 };
