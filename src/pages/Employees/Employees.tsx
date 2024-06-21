384a770c3ab7b790baec7212582c80f73f4540d3
Besnier Lucien <lucien.besnier@gmail.com>
2024-06-21 15:51:22 +0200

    [ADD] All employee tab

--- src/pages/Employees/Employees.tsx
+++ src/pages/Employees/Employees.tsx
@@ -1,21 +1,101 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { IonContent, IonPage } from '@ionic/react';
-import HeaderEmployee from '../../components/Header/Employee/HeaderEmployee';
-import './Employees.scss';
-import { jwtDecode } from 'jwt-decode';
+import { IonBreadcrumb, IonBreadcrumbs, IonContent, IonPage, IonSearchbar } from '@ionic/react';
 import HeaderAdmin from '../../components/Header/Admin/HeaderAdmin';
+import './Employees.scss';
+import { getEmployeeAll } from '../../api/employee/getEmployeeAll';
+import { jwtDecode } from 'jwt-decode';
+
+// Définissez l'interface Employee
+interface Employee {
+  name: string;
+  surname: string;
+  [key: string]: any; // Vous pouvez ajouter d'autres propriétés si nécessaire
+}
 
 const Employees: React.FC = () => {
- 
+  const history = useHistory();
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const [filteredEmployees, setFilteredEmployees] = useState<Employee[]>([]);
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      const decodedToken: any = jwtDecode(token);
+      if (decodedToken.position === "RH") {
+        setIsAdmin(true);
+      } else {
+        history.push('/auth');
+      }
+    } else {
+      history.push('/auth');
+    }
+  }, [history]);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const token = localStorage.getItem('access_token');
+        if (token) {
+          const employeeData = await getEmployeeAll(token) as Employee[];
+          const sortedData = employeeData.sort((a, b) => {
+            const nameA = `${a.name} ${a.surname}`.toLowerCase();
+            const nameB = `${b.name} ${b.surname}`.toLowerCase();
+            return nameA.localeCompare(nameB);
+          });
+          setEmployees(sortedData);
+          setFilteredEmployees(sortedData);
+        } else {
+          history.push('/auth');
+        }
+      } catch (error) {
+        console.error('Error fetching employees:', error);
+      }
+    };
+
+    if (isAdmin) {
+      fetchEmployees();
+    }
+  }, [isAdmin, history]);
+
+  useEffect(() => {
+    const filteredData = employees.filter(employee => 
+      `${employee.name} ${employee.surname}`.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredEmployees(filteredData);
+  }, [searchText, employees]);
 
   return (
     <IonPage>
       <HeaderAdmin />
+      <IonBreadcrumbs style={{ marginTop: '2em', marginLeft: '10px' }}>
+        <IonBreadcrumb href="/homepage-admin">Home</IonBreadcrumb>
+        <IonBreadcrumb href="/employees">Mes employées</IonBreadcrumb>
+      </IonBreadcrumbs>
       <IonContent>
-        <div>
-          hello
-        </div>
+        <IonSearchbar 
+          value={searchText}
+          onIonChange={e => setSearchText(e.detail.value!)}
+          debounce={300}
+          placeholder="Rechercher par nom ou prénom" autocapitalize={''}        />
+        <table>
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Prénom</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredEmployees.map((employee, index) => (
+              <tr key={index}>
+                <td>{employee.name}</td>
+                <td>{employee.surname}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </IonContent>
     </IonPage>
   );
